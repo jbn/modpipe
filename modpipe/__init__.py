bf2ffc3e6f4f32f9db6c4d5da50d9b009832f6c4
= <jbn@abreka.com>
2018-07-24 13:26:42 -0700

    Bump version

--- modpipe/__init__.py
+++ modpipe/__init__.py
@@ -3,4 +3,4 @@ from modpipe.modpipe_impl import ModPipe, Result, Done, SkipTo  # noqa: F401
 
 __author__ = """John Bjorn Nelson"""
 __email__ = 'jbn@abreka.com'
-__version__ = '0.0.3'
+__version__ = '0.0.4'
